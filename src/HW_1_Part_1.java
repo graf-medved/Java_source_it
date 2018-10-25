39b4c9e26df7b50aaf73809df37d2375fc95eb10
bir alex <bir@yandex.ru>
2018-10-25 19:57:03 +0300

    Init commit

---
+++ src/HW_1_Part_1.java
@@ -0,0 +1,16 @@
+
+//public class HW_1_Part_1 {
+//	public static void main(String args[]) {
+//		Systen.out.println("Hellow Java")
+//}
+
+/*
+Это простая программа Java.
+Назовите этот файл "Example.java".
+*/
+class Example {
+// Программа начинается с обращения к main().
+public static void main(String args[]) {
+System.out.println("Простая программа Java.");
+}
+}
